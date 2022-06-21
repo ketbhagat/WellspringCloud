b8c0accf63fa261b6da6bc711ffe0df9702f78d2
Ketan Bhagat <ketanbhagat@live.com>
2022-06-20 22:47:05 -0400

    Commit-04

--- ec2.tf
+++ ec2.tf
@@ -3,9 +3,12 @@ resource "aws_instance" "web" {
 	ami = data.aws_ami.awslinux2.id
 	instance_type = "t2.micro"
 	subnet_id = aws_subnet.public_1a.id
-	security_groups = ["allow_tls"]
+	vpc_security_group_ids = [ "aws_security_group.allow_tls.id" ]
 	key_name = var.privatekey
 	tags = {
 		Name = "WSC-Web"
 	}
+	depends_on = [
+		aws_security_group.allow_tls
+	]
 }
