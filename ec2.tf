e710ebab7326f26ed16ced805a2df00184f0accb
Ketan Bhagat <88006325+ketbhagat@users.noreply.github.com>
2022-06-23 10:54:22 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,9 @@
 #EC2 Instance
 resource "aws_instance" "bastion_host" {
+	count = "${length(var.web_subnets_cidr_blocks)}"
 	ami = data.aws_ami.awslinux2.id
 	instance_type = "t2.micro"
-	subnet_id = aws_subnet.public.id
+	subnet_id = "${element(aws_subnet.web.*.id,count.index)}"
 	vpc_security_group_ids = [ aws_security_group.bastion_host.id ]
 	user_data = <<EOF
 		#!/bin/bash
@@ -12,6 +13,6 @@ resource "aws_instance" "bastion_host" {
 		EOF
 	key_name = var.privatekey
 	tags = {
-		Name = "WSC-Bastion_Host"
+		Name = "${element(WSC-Bastion_Host,count.index)}"
 	}
 }
