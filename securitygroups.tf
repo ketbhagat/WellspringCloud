23f8d053d522dd0ce9a758a8228de19605817838
Ketan Bhagat <88006325+ketbhagat@users.noreply.github.com>
2022-06-23 06:24:00 -0400

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -1,11 +1,93 @@
 #Security Group
-resource "aws_security_group" "allow_tls" {
-	name = "allow_tls"
-	description = "Allow TLS inbound traffic"
+resource "aws_security_group" "bastion_host" {
+	name = "WSC-bastion_host"
+	description = "Allow SSH inbound traffic"
 	vpc_id = aws_vpc.main.id
 
 	dynamic "ingress" {
 		for_each = var.sg_bastion_host
+		iterator = port
+		content {
+			description = "SSH from VPC"
+			from_port =  port.value
+			to_port =  port.value
+			protocol = "tcp"
+			cidr_blocks = ["0.0.0.0/0"]
+		}
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol         = "-1"
+		cidr_blocks      = ["0.0.0.0/0"]
+	}
+	tags = {
+		Name = "WSC-bastion_host"
+	}
+}
+
+resource "aws_security_group" "weblb" {
+	name = "WSC-WebLB"
+	description = "Allow http inbound traffic"
+	vpc_id = aws_vpc.main.id
+
+	dynamic "ingress" {
+		for_each = var.sg_weblb
+		iterator = port
+		content {
+			description = "HTTP from VPC"
+			from_port =  port.value
+			to_port =  port.value
+			protocol = "tcp"
+			cidr_blocks = ["0.0.0.0/0"]
+		}
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol         = "-1"
+		cidr_blocks      = ["0.0.0.0/0"]
+	}
+	tags = {
+		Name = "WSC-WebLB"
+	}
+}
+
+
+resource "aws_security_group" "webserver" {
+	name = "WSC-WebServer"
+	description = "Allow http inbound traffic"
+	vpc_id = aws_vpc.main.id
+
+	dynamic "ingress" {
+		for_each = var.sg_webserver
+		iterator = port
+		content {
+			description = "HTTP from VPC"
+			from_port =  port.value
+			to_port =  port.value
+			protocol = "tcp"
+			cidr_blocks = ["0.0.0.0/0"]
+		}
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol         = "-1"
+		cidr_blocks      = ["0.0.0.0/0"]
+	}
+	tags = {
+		Name = "WSC-WebServer"
+	}
+}
+
+resource "aws_security_group" "appserver" {
+	name = "WSC-AppServer"
+	description = "Allow http inbound traffic"
+	vpc_id = aws_vpc.main.id
+
+	dynamic "ingress" {
+		for_each = var.sg_appserver
 		iterator = port
 		content {
 			description = "TLS from VPC"
@@ -22,6 +104,33 @@ resource "aws_security_group" "allow_tls" {
 		cidr_blocks      = ["0.0.0.0/0"]
 	}
 	tags = {
-		Name = "allow_tls"
+		Name = "WSC-AppServer"
+	}
+}
+
+resource "aws_security_group" "db" {
+	name = "WSC-DB"
+	description = "Allow 3306 inbound traffic"
+	vpc_id = aws_vpc.main.id
+
+	dynamic "ingress" {
+		for_each = var.sg_db
+		iterator = port
+		content {
+			description = "TLS from VPC"
+			from_port =  port.value
+			to_port =  port.value
+			protocol = "tcp"
+			cidr_blocks = ["0.0.0.0/0"]
+		}
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol         = "-1"
+		cidr_blocks      = ["0.0.0.0/0"]
+	}
+	tags = {
+		Name = "WSC-DB"
 	}
 }
